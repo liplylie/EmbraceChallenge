1ec2f47426ca9c164ded0471dfc8619b3293831e
Jordan Daniels <decentpianist@gmail.com>
2017-12-02 21:07:27 -0800

    barebone set up with react, react-redux, webpack, and express complete

---
+++ server/router/router.js
@@ -0,0 +1,5 @@
+const app = require('../server.js')
+const router = require('express').Router()
+//const controller = require('../controller/controller.js')
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+//const router = require('./router/router.js')
+const app = express()
+let port = process.env.PORT || 3000;
+let ip = 'localhost'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'../client/static')))
+//app.use('/api', router)
+
+app.listen(port, function(){
+	console.log("server is listening on port " + port)
+})
+
+module.exports = app;
