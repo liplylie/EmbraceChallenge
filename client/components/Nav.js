6517d6a6840b5e2f1879dd789b565920ec040069
Jordan Daniels <decentpianist@gmail.com>
2017-12-02 23:44:53 -0800

    basic nav bar and home page are set up. Need to made side bar for dog list

---
+++ client/components/Nav.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="fixed-top" style={{backgroundColor: 'white'}}>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container">
+            <span className="navbar-brand mb-0">
+              <Link to='/' className="nav-link">
+                DogLog
+              </Link>
+            </span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/favoriteDogs" className="nav-link">Favorite Dogs</Link>
+                </li>
+                {/* <li className="nav-item">
+                  <a href="#" className="nav-link">Login</a>
+                </li> */}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Nav;
---
+++ client/components/SideNav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SideNav extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+    	<div>
+    	SideNav
+    	</div>
+    )
+  }
+}
