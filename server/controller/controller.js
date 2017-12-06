08726ce4aabeeecdf7ca5392d277a1cb6d066930
Jordan Daniels <decentpianist@gmail.com>
2017-12-05 19:11:12 -0800

    clicking on add favorite dog creates a new entry in the favorite dog view

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,1 +1,24 @@
 const router = require('../router/router.js')
+const favoriteDog = require('../../db/models/favoriteDogModel.js')
+
+
+module.exports = {
+	getFavoriteDogs: function(req,res){
+		favoriteDog.findAll().then((dogs) => {
+  			res.status(200).send(JSON.stringify(dogs))
+		})
+		.catch(()=>{console.log("get dogs failed")})
+	},
+	addFavoriteDog: function(req,res){
+		console.log(req.body, "post res")
+		favoriteDog.create({
+        dogName: req.body.dogName,
+        dogImage: req.body.dogImage
+    }).then(function(data) {
+       	console.log(data, "post data")
+        res.status(200).send(data)
+
+	})
+	.catch(()=>{console.log("add fav dog failed")})
+	}
+}
